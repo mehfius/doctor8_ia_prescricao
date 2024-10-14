863f6140aa78e6e47f8b70c65287018432ce2927
matheus ferraz <matheus.ferraz@gmail.com>
2024-10-14 08:56:10 -0300

    Mudança para Open_ai

--- text_to_ia.js
+++ text_to_ia.js
@@ -6,28 +6,48 @@ const text_to_ia = async function (prompt) {
 
     while (!success) {
         try {
-            const response = await fetch('https://open-pumped-lacewing.ngrok-free.app/api/generate', {
+/*             const response = await fetch('https://open-pumped-lacewing.ngrok-free.app/api/generate', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
-                    model: "llama3.1",
+                    model: "llama3.2",
                     prompt: `${prompt}`,
                     stream: false,
-                    //temperature: 0.3
-                })
-            });                           
+                    temperature: 0.1
+                }) 
+            });       */   
+
+            const response = await fetch('https://api.openai.com/v1/chat/completions', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization' : 'Bearer ' + process.env.OPENAI_KEY,
+                },
+                body: JSON.stringify({
+                    model: "gpt-4o-mini",
+                    messages: [
+                        {
+                            role: "user",
+                            content: `${prompt}`
+                        }
+                    ],
+                    temperature: 0.7
+                }) 
+            });      
 
             r = await response;
            
             if(r.status == 200){
-
+               
                 const IAServerResponse = await r.json();
+
+                const open_ai_response = IAServerResponse.choices[0].message.content;         
               
                 // Extract the array of objects from the response text
-                const matches = IAServerResponse.response.match(/\[.*?\]/s);      
-                
+                const matches = open_ai_response.match(/\[.*?\]/s);      
+ 
                 if (matches && matches.length > 0) {
                     responseObject = JSON.parse(matches[0]);
                     
@@ -44,7 +64,7 @@ const text_to_ia = async function (prompt) {
                 }
 
             } else {
-                console.error('Ngrok Offline');
+                console.error('Offline');
             }                   
 
             success = true;
